ed140c64271d9ad5f0b42fbba9f967edd616a9db
Dmitri Carapezza <dmitricarapezza@gmail.com>
2015-01-19 11:11:23 -0700

    initial commit

---
+++ inc/footer.php
@@ -0,0 +1,38 @@
+    <div class="footer">
+        <div class="container">
+			<div class="col-md-6">
+				<a href="#"><img src="img/footer-logo.png" class="logo" /></a>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Our Approach</a></li>
+			        <li><a href="#">Services</a></li>
+			        <li><a href="#">Blog</a></li>
+			        <li><a href="#">Talent Network</a></li>
+			        <li><a href="#">Contact</a></li>
+				</ul>
+				<p class="copyright">&copy; <?php echo date("Y") ?> Extreme Technologies Inc. - Houston, Texas - All Rights Reserved.</p>
+			</div>
+			<div class="col-md-4">
+				<h3 class="white bold">281.293.7800</h3>
+				<p class="white bold">
+					<a href="mailto:sales@extreme-technologies.com">sales@extreme-technologies.com</a>
+						<br>
+					<a href="mailto:recruiting@extreme-technologies.com">recruiting@extreme-technologies.com</a>
+				</p>
+			</div>
+			<div class="col-lg-2">
+				<a href="#"><img src="img/icon-google-white.png" class="social" />
+				<a href="#"><img src="img/icon-twitter-white.png" class="social" />
+				<a href="#"><img src="img/icon-linkedin-white.png" class="social" />
+			</div>
+        </div>     
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+ </body>
+</html>
